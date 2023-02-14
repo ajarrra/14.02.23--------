572a481c4530f4c89bb83fbcfd9aeda4cfa43eb9
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-14 14:56:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//создали обьект
+let example = {};
+let ajar = {
+    name: "Elya",
+    age: 28,
+    isEmployed: false
+
+};         //может юыть хоть скока свойств
+
+
+let Ajar = {
+    name: "Ajar",
+    age: 18,
+    height: 165,
+    weight: 53,
+    isEmployed: true
+}
