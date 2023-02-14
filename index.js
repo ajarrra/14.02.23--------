eb4b3ce3d24c2f9e670ac0e5d13b8fe7cde24fa5
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-14 15:02:39 +0100

    removed the object

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //создали обьект
-let example = {};
-let ajar = {
+let example = {};//пустой обьект
+let Elya = {
     name: "Elya",
     age: 28,
     isEmployed: false
@@ -14,4 +14,15 @@ let Ajar = {
     height: 165,
     weight: 53,
     isEmployed: true
-}
+};
+
+let Nestana = {
+    name: "NEstana",
+    age: 56,
+    weight: 52
+};
+
+// view , edit , add
+console.log (Ajar.age);
+console.log (Elya.name);
+console.log (Nestana.weight );
